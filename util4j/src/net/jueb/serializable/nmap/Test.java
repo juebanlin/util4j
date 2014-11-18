1727fa5f3663c508672fc942697d38fa40a7873c
Administrator <Administrator@juebanlin-PC.mshome.net>
2014-11-18 20:59:36 +0800

    Signed-off-by: Administrator <Administrator@juebanlin-PC.mshome.net>

--- util4j/src/net/jueb/serializable/nmap/Test.java
+++ util4j/src/net/jueb/serializable/nmap/Test.java
@@ -1,11 +1,9 @@
 package net.jueb.serializable.nmap;
 
 import java.util.Arrays;
-import java.util.HashMap;
 import net.jueb.serializable.nmap.type.NBoolean;
 import net.jueb.serializable.nmap.type.NInteger;
 import net.jueb.serializable.nmap.type.NMap;
-import net.jueb.serializable.nmap.type.NType;
 import net.jueb.serializable.nmap.type.NUTF8String;
 
 public class Test {
@@ -21,11 +19,10 @@ public class Test {
 		NInteger ageValue=new NInteger(18);
 		NUTF8String isWorkKey=new NUTF8String("isWork");
 		NBoolean isWorkValue=new NBoolean(true);
-		HashMap<NType<?>,NType<?>> map=new HashMap<NType<?>, NType<?>>();
-		map.put(nameKey, nameValue);
-		map.put(ageKey, ageValue);
-		map.put(isWorkKey, isWorkValue);
-		NMap nmap=new NMap(map);
+		NMap nmap=new NMap();
+		nmap.put(nameKey, nameValue);
+		nmap.put(ageKey, ageValue);
+		nmap.put(isWorkKey, isWorkValue);
 		byte[] data=nmap.getBytes();
 		System.out.println(nmap.toString());
 		System.out.println(Arrays.toString(data));
--- util4j/src/net/jueb/serializable/nmap/type/NMap.java
+++ util4j/src/net/jueb/serializable/nmap/type/NMap.java
@@ -1,12 +1,20 @@
 package net.jueb.serializable.nmap.type;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import net.jueb.serializable.nmap.falg.Flag;
 
-public class NMap extends NType<Map<NType<?>,NType<?>>>{
+/**
+ * 内部封装一个hashmap
+ * @author Administrator
+ *
+ */
+public class NMap extends NType<Map<NType<?>,NType<?>>> implements Map<NType<?>, NType<?>>{
 
-	public NMap(Map<NType<?>, NType<?>> obj) {
-		super(obj, Flag.Head.NMap, Flag.End.NMap);
+	public NMap() {
+		super(new HashMap<NType<?>, NType<?>>(), Flag.Head.NMap, Flag.End.NMap);
 	}
 	
 	@Override
@@ -24,10 +32,70 @@ public class NMap extends NType<Map<NType<?>,NType<?>>>{
 		}
 		return data;
 	}
-
+	
 	@Override
 	public String getString() {
 		return obj.toString();
+	}
+
+	@Override
+	public int size() {
+		return obj.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return obj.isEmpty();
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		return obj.containsKey(key);
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		return obj.containsValue(value);
+	}
+
+	@Override
+	public NType<?> get(Object key) {
+		return obj.get(key);
+	}
+
+	@Override
+	public NType<?> put(NType<?> key, NType<?> value) {
+		return obj.put(key, value);
+	}
+
+	@Override
+	public NType<?> remove(Object key) {
+		return obj.remove(key);
+	}
+
+	@Override
+	public void putAll(Map<? extends NType<?>, ? extends NType<?>> m) {
+		obj.putAll(m);
+	}
+
+	@Override
+	public void clear() {
+		obj.clear();
+	}
+
+	@Override
+	public Set<NType<?>> keySet() {
+		return obj.keySet();
+	}
+
+	@Override
+	public Collection<NType<?>> values() {
+		return obj.values();
+	}
+
+	@Override
+	public Set<java.util.Map.Entry<NType<?>, NType<?>>> entrySet() {
+		return obj.entrySet();
 	}
 
 }
