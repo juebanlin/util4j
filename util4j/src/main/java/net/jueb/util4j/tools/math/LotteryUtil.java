d839c99b29076dc914a5d0afdc19dace18e8599a
Administrator <Administrator@WIN-1N08606L804>
2015-04-08 17:14:26 +0800

    Signed-off-by: Administrator <Administrator@WIN-1N08606L804>

--- util4j/src/main/java/net/jueb/util4j/tools/math/LotteryUtil.java
+++ util4j/src/main/java/net/jueb/util4j/tools/math/LotteryUtil.java
@@ -1,10 +1,10 @@
 package net.jueb.util4j.tools.math;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 /**
  * 概率随机工具
@@ -40,65 +40,36 @@ public class LotteryUtil {
 	 * @param lotterItems
 	 * @return
 	 */
-	public static <M> M lotteryItem(List<LotterItem<M>> lotterItems)
+	public static <M> LotterItem<M> lotteryItem(List<LotterItem<M>> lotterItems)
 	{ 	if (lotterItems == null || lotterItems.isEmpty()) {
 			return null;
     	}
-    	int size = lotterItems.size();
+		LotterItem<M> result=null;
     	// 计算总概率，这样可以保证不一定总概率是1
     	double sumProbability = 0d;
-    	for (LotterItem<M> item : lotterItems) {
+    	for (LotterItem<M> item : lotterItems) 
+    	{
     		sumProbability += item.getProbability();
     	}
-
+    	double nextDouble = Math.random()/sumProbability;//随机一个概率值
     	// 计算每个物品在总概率的基础下的概率情况
-    	List<Double> sortOrignalRates = new ArrayList<Double>(size);
     	Double tempSumRate = 0d;
-    	for (LotterItem<M> item: lotterItems) {
+    	for (LotterItem<M> item: lotterItems) 
+    	{
     		tempSumRate += item.getProbability();
-    		sortOrignalRates.add(tempSumRate / sumProbability);
+    		if(nextDouble<=tempSumRate)
+    		{// 根据区块值来获取抽取到的物品索引
+    			result=item;
+    			break;
+    		}
     	}
-    	// 根据区块值来获取抽取到的物品索引
-    	double nextDouble = Math.random();//随机一个小数
-    	sortOrignalRates.add(nextDouble);//把小数加入到集合
-    	Collections.sort(sortOrignalRates);
-    	int index=sortOrignalRates.indexOf(nextDouble);//概率索引
-    	return lotterItems.get(index).getItem();
+    	if(result==null)
+    	{
+    		result=lotterItems.get(new Random().nextInt(lotterItems.size()));
+    	}
+    	return result;
 	}
 	
-    /**
-     * 随机概率
-     * @param orignalRates
-     *            原始的概率列表，保证顺序和实际物品对应
-     * @return
-     * 概率的索引
-     */
-    public static int lottery(List<Double> orignalRates) {
-        if (orignalRates == null || orignalRates.isEmpty()) {
-            return -1;
-        }
-        int size = orignalRates.size();
-
-        // 计算总概率，这样可以保证不一定总概率是1
-        double sumRate = 0d;
-        for (double rate : orignalRates) {
-            sumRate += rate;
-        }
-
-        // 计算每个物品在总概率的基础下的概率情况
-        List<Double> sortOrignalRates = new ArrayList<Double>(size);
-        Double tempSumRate = 0d;
-        for (double rate : orignalRates) {
-            tempSumRate += rate;
-            sortOrignalRates.add(tempSumRate / sumRate);
-        }
-
-        // 根据区块值来获取抽取到的物品索引
-        double nextDouble = Math.random();//随机一个小数
-        sortOrignalRates.add(nextDouble);//把小数加入到集合
-        Collections.sort(sortOrignalRates);
-        return sortOrignalRates.indexOf(nextDouble);
-    }
     public static void main(String[] args) {
 		ArrayList<LotterItem<FruitType>> fruits=new ArrayList<LotteryUtil.LotterItem<FruitType>>();
 		LotterItem<FruitType> fruit1=new LotterItem<FruitType>(FruitType.Apple, 0.296666667);
@@ -115,7 +86,7 @@ public class LotteryUtil {
 		Map<FruitType,Integer> map=new HashMap<FruitType, Integer>();
 		for(int i=0;i<count;i++)
 		{
-			FruitType item=LotteryUtil.lotteryItem(fruits);
+			FruitType item=LotteryUtil.lotteryItem(fruits).getItem();
 			if(map.containsKey(item))
 			{
 				int tmp=map.get(item);
