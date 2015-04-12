cbb91bd72a12227deca2f14910790af374f7f17d
Administrator <Administrator@192.168.1.6>
2015-04-12 21:51:34 +0800

    Signed-off-by: Administrator <Administrator@192.168.1.6>

--- util4j/src/main/java/net/jueb/util4j/classLoader/loader/MyUrlClassLoader.java
+++ util4j/src/main/java/net/jueb/util4j/classLoader/loader/MyUrlClassLoader.java
@@ -1,11 +1,7 @@
 package net.jueb.util4j.classLoader.loader;
 
-import java.io.File;
-import java.lang.reflect.InvocationTargetException;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.util.Arrays;
 
 /**
  *@author juebanlin
