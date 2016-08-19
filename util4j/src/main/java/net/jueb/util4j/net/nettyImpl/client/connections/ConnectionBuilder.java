cbcca75e0b4b9746402bf3aa35691065681707b5
juebanlin <juebanlin@DESKTOP-68KCVE3>
2016-08-19 10:23:34 +0800

    Signed-off-by: juebanlin <juebanlin@DESKTOP-68KCVE3>

--- util4j/src/main/java/net/jueb/util4j/net/nettyImpl/client/connections/ConnectionBuilder.java
+++ util4j/src/main/java/net/jueb/util4j/net/nettyImpl/client/connections/ConnectionBuilder.java
@@ -2,13 +2,33 @@ package net.jueb.util4j.net.nettyImpl.client.connections;
 
 import java.net.InetSocketAddress;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import net.jueb.util4j.net.nettyImpl.client.MultiNettyClientConfig;
 
 public class ConnectionBuilder extends MultiNettyClientConfig{
 	
+	public ConnectionBuilder() {
+		super();
+	}
+
+	public ConnectionBuilder(Class<? extends SocketChannel> channelClass, EventLoopGroup ioWorkers) {
+		super(channelClass, ioWorkers);
+	}
+
+	public ConnectionBuilder(int ioThreads) {
+		super(ioThreads);
+	}
+
 	public final ChannelFuture connect(InetSocketAddress address)
 	{
 		return doBooterConnect(address, null);
+	}
+	
+	public final ChannelFuture connect(InetSocketAddress address,ChannelHandler handler)
+	{
+		return doBooterConnect(address,handler);
 	}
 	
 	public static void main(String[] args) {
