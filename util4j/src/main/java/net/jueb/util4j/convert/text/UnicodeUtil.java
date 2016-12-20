30843a6daff0ab5c87af9851779a67985ee0e983
Administrator <Administrator@XB-20150411SHYD>
2016-12-20 10:49:43 +0800

    Signed-off-by: Administrator <Administrator@XB-20150411SHYD>

---
+++ util4j/src/main/java/net/jueb/util4j/convert/text/UnicodeUtil.java
@@ -0,0 +1,29 @@
+package net.jueb.util4j.convert.text;
+
+public class UnicodeUtil {
+
+	/**
+	 * unicode 转字符串
+	 */
+	public static String unicode2String(String unicode) {
+	    StringBuffer string = new StringBuffer();
+	    String[] hex = unicode.split("\\\\u");
+	    for (int i = 1; i < hex.length; i++) {
+	        int data = Integer.parseInt(hex[i], 16);
+	        string.append((char) data);
+	    }
+	    return string.toString();
+	}
+	
+	/**
+	 * 字符串转换unicode
+	 */
+	public static String string2Unicode(String string) {
+	    StringBuffer unicode = new StringBuffer();
+	    for (int i = 0; i < string.length(); i++) {
+	        char c = string.charAt(i);
+	        unicode.append("\\u" + Integer.toHexString(c));
+	    }
+	    return unicode.toString();
+	}
+}
