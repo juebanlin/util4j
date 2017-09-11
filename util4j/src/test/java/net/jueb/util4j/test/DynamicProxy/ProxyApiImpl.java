9e65a92884f382723f8da6f37bded664e4a1d4b8
juebanlin <juebanlin@DESKTOP-IBGCEI5>
2017-09-11 13:54:17 +0800

    Signed-off-by: juebanlin <juebanlin@DESKTOP-IBGCEI5>

--- util4j/src/test/java/net/jueb/util4j/test/DynamicProxy/ProxyApiImpl.java
+++
@@ -1,9 +0,0 @@
-package net.jueb.util4j.test.DynamicProxy;
-
-public class ProxyApiImpl implements ProxyApi{
-	
-    public int test(int i) {
-    	System.out.println(i);
-        return i+1;
-    }
-}
--- util4j/src/test/java/net/jueb/util4j/test/DynamicProxy/ProxyUtil.java
+++ util4j/src/test/java/net/jueb/util4j/test/DynamicProxy/ProxyUtil.java
@@ -6,71 +6,32 @@ import java.lang.reflect.Proxy;
 
 public class ProxyUtil {
 
+	/**
+	 * 创建代理对象
+	 * @param loader
+	 * @param proxyInterface
+	 * @param proxyImpl
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T newProxyInstance(ProxyHandle<T> handler) {
-        return (T) Proxy.newProxyInstance(handler.getClass().getClassLoader(),
-                        new Class<?>[] { handler.getProxyInterface() },
-                       new InvocationHandlerImpl<T>(handler.getProxyTarget(),handler));
-    }
-	
-	private static class InvocationHandlerImpl<T> implements InvocationHandler{
-		private final ProxyHandle<T> handler;
-		public InvocationHandlerImpl(T proxyTarget,ProxyHandle<T> handler) {
-			this.handler=handler;
-		}
-		
-		@SuppressWarnings("unchecked")
-		@Override
-		public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-			return handler.invoke((T) proxy, method, args);
-		}
+	public static <T> T newProxyInstance(ClassLoader loader,Class<T> proxyInterface,T proxyImpl) {
+		return (T) Proxy.newProxyInstance(loader,
+                new Class<?>[] { proxyInterface },
+               new InvocationHandler() {
+				@Override
+				public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+					return method.invoke(proxyImpl, args);
+				}
+                });
 	}
 	
-	public static interface ProxyHandle<T>{
-		
-		/**
-		 * 代理类
-		 * @param proxy
-		 * @param method
-		 * @param args
-		 * @return
-		 * @throws Throwable
-		 */
-		public Object invoke(T proxy, Method method, Object[] args)
-		        throws Throwable;
-		
-		/**
-		 * 代理对象
-		 * @return
-		 */
-		public T getProxyTarget();
-		
-		/**
-		 * 代理对象
-		 * @return
-		 */
-		public Class<T> getProxyInterface();
-		
-	}
 	public static void main(String[] args) {
-		ProxyUtil.newProxyInstance(new ProxyHandle<ProxyApi>(){
-
+		Runnable run=new Runnable() {
 			@Override
-			public Object invoke(ProxyApi proxy, Method method, Object[] args) throws Throwable {
-				return method.invoke(getProxyTarget(), args);
+			public void run() {
+				System.out.println("runImpl");
 			}
-
-			@Override
-			public ProxyApi getProxyTarget() {
-				return new ProxyApiImpl();
-			}
-
-			@Override
-			public Class<ProxyApi> getProxyInterface() {
-				return ProxyApi.class;
-			}
-			
-		}).test(1);
+		};
+		Runnable run1=ProxyUtil.newProxyInstance(run.getClass().getClassLoader(),Runnable.class,run);
 	}
-	
 }
