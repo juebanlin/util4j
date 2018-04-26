2e6409effe952094a6b6d33b2db3b61ea25ea7b7
jaci <jaci@DESKTOP-6IB2KSJ>
2018-04-26 14:43:25 +0800

    websocket ssl fix

--- util4j/src/main/java/net/jueb/util4j/net/nettyImpl/client/websocket/NettyWebSocketClient.java
+++ util4j/src/main/java/net/jueb/util4j/net/nettyImpl/client/websocket/NettyWebSocketClient.java
@@ -2,12 +2,11 @@ package net.jueb.util4j.net.nettyImpl.client.websocket;
 import java.net.URI;
 
 import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.SslContext;
 import net.jueb.util4j.net.nettyImpl.client.NettyClient;
 import net.jueb.util4j.net.nettyImpl.client.NettyClientConfig;
-import net.jueb.util4j.net.nettyImpl.handler.websocket.WebSocketClientInitializer;
+import net.jueb.util4j.net.nettyImpl.handler.websocket.DefaultWebSocketClientInitializer;
 
 /**
  * websocket客户端 
@@ -47,15 +46,7 @@ public class NettyWebSocketClient extends NettyClient {
 	 */
 	@Override
 	protected ChannelHandler fixHandlerBeforeConnect(final ChannelHandler handler) {
-		ChannelHandler result=new WebSocketClientInitializer(webSocketURL, sslCtx) {
-			@Override
-			protected void webSocketHandComplete(ChannelHandlerContext ctx) {
-				ctx.channel().pipeline().addLast(handler);
-				//为新加的handler手动触发必要事件
-				ctx.fireChannelRegistered();
-				ctx.fireChannelActive();
-			}
-		};
+		ChannelHandler result=new DefaultWebSocketClientInitializer(webSocketURL, sslCtx,handler);
 		return result;
 	}	
 }
--- util4j/src/main/java/net/jueb/util4j/net/nettyImpl/server/NettyWebSocketServer.java
+++ util4j/src/main/java/net/jueb/util4j/net/nettyImpl/server/NettyWebSocketServer.java
@@ -3,10 +3,9 @@ package net.jueb.util4j.net.nettyImpl.server;
 import java.net.InetSocketAddress;
 
 import io.netty.channel.ChannelHandler;
-import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.handler.ssl.SslContext;
-import net.jueb.util4j.net.nettyImpl.handler.websocket.WebSocketServerInitializer;
+import net.jueb.util4j.net.nettyImpl.handler.websocket.DefaultWebSocketServerInitializer;
 
 /**
  * 将WebSocket的Binary流转换为正常socket链路的服务器
@@ -47,15 +46,7 @@ public class NettyWebSocketServer extends NettyServer{
 	 */
 	@Override
 	protected ChannelHandler fixHandlerBeforeDoBooterBind(ChannelHandler handler) {
-		ChannelHandler result=new WebSocketServerInitializer(websocketPath, sslCtx) {
-			@Override
-			protected void webSocketHandComplete(ChannelHandlerContext ctx) {
-				ctx.channel().pipeline().addLast(handler);
-				//为新加的handler手动触发必要事件
-				ctx.fireChannelRegistered();
-				ctx.fireChannelActive();
-			}
-		};
+		ChannelHandler result=new DefaultWebSocketServerInitializer(websocketPath, sslCtx,handler);
 		return result;
 	}
 }
