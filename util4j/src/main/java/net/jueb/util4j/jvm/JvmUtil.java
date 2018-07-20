37107fa27f76e00c1fc1e4a083ca7ce0b8db8a5b
何林 <juebanlin@gmail.com>
2018-07-20 17:28:56 +0800

    remove tools dependencies,for java10 support

--- util4j/src/main/java/net/jueb/util4j/jvm/JvmUtil.java
+++ util4j/src/main/java/net/jueb/util4j/jvm/JvmUtil.java
@@ -1,10 +1,6 @@
 package net.jueb.util4j.jvm;
 
-import java.io.IOException;
 import java.lang.management.ManagementFactory;
-
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
 
 public class JvmUtil {
 	
@@ -20,21 +16,5 @@ public class JvmUtil {
 			pid = pid.substring(0, indexOf);
 		}
 		return Integer.parseInt(pid);
-	}
-	
-	/**
-	 * 获取当前JVM
-	 * @return
-	 * @throws IOException 
-	 * @throws AttachNotSupportedException 
-	 */
-	public static VirtualMachine getVirtualMachine() throws AttachNotSupportedException, IOException
-	{
-		String pid = ManagementFactory.getRuntimeMXBean().getName();
-		int indexOf = pid.indexOf('@');
-		if (indexOf > 0) {
-			pid = pid.substring(0, indexOf);
-		}
-		return VirtualMachine.attach(pid);
 	}
 }
