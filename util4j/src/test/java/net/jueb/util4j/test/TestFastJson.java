37a0edf1dbd85291cff104fa237fc19f5ad6ac4b
helin <juebanlin@gmail.com>
2019-03-27 15:40:54 +0800

    1
    Signed-off-by: helin <juebanlin@gmail.com>

--- util4j/src/test/java/net/jueb/util4j/test/TestFastJson.java
+++ util4j/src/test/java/net/jueb/util4j/test/TestFastJson.java
@@ -1,9 +1,12 @@
 package net.jueb.util4j.test;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
-import com.google.gson.JsonArray;
+import java.lang.reflect.Type;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.TypeReference;
+import com.alibaba.fastjson.parser.DefaultJSONParser;
 public class TestFastJson {
 
 	private int id=1;
@@ -36,14 +39,23 @@ public class TestFastJson {
 	public static void main(String[] args) {
 		TestFastJson entity=new TestFastJson();
 		JSONObject json=new JSONObject();
-		JsonArray list=new JsonArray();
+		JSONArray list=new JSONArray();
 		list.add(1);
 		json.put("1", 1);
 		json.put("list",list);
 		entity.setJson(json);
-		String str=JSON.toJSONString(entity);
-		System.out.println(str);
-		entity=JSONObject.parseObject(str,TestFastJson.class);
+		String jsonStr=JSON.toJSONString(entity);
+		System.out.println(jsonStr);
+		entity=JSONObject.parseObject(jsonStr,TestFastJson.class);
 		System.out.println(entity);
+		//反序列化
+		DefaultJSONParser jp=new DefaultJSONParser(jsonStr);
+		JSONObject json2=jp.parseObject();
+		System.out.println("id："+json2.getIntValue("id"));
+		//类型反序列化
+		Type type=new TypeReference<TestFastJson>() {
+		}.getType();
+		TestFastJson entity2=JSON.parseObject(jsonStr, type);
+		System.out.println(entity2.getId());
 	}
 }
