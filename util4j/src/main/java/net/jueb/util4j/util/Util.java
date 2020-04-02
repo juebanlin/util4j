8d811616c0fe90949746552fd8d0a8df0e7d9261
helin <juebanlin@gmail.com>
2020-04-02 15:45:56 +0800

    1

--- util4j/src/main/java/net/jueb/util4j/util/Util.java
+++ util4j/src/main/java/net/jueb/util4j/util/Util.java
@@ -12,4 +12,29 @@ public class Util {
 	{
 		return (number & (0x1<<pos))>>>pos;
 	}
+
+	public static boolean isInRange(String ip, String cidr) {
+		String[] ips = ip.split("\\.");
+		int ipAddr = (Integer.parseInt(ips[0]) << 24)
+				| (Integer.parseInt(ips[1]) << 16)
+				| (Integer.parseInt(ips[2]) << 8)
+				| Integer.parseInt(ips[3]);
+		int type = Integer.parseInt(cidr.replaceAll(".*/", ""));
+		int mask = 0xFFFFFFFF << (32 - type);
+		String cidrIp = cidr.replaceAll("/.*", "");
+		String[] cidrIps = cidrIp.split("\\.");
+		int cidrIpAddr = (Integer.parseInt(cidrIps[0]) << 24)
+				| (Integer.parseInt(cidrIps[1]) << 16)
+				| (Integer.parseInt(cidrIps[2]) << 8)
+				| Integer.parseInt(cidrIps[3]);
+
+		return (ipAddr & mask) == (cidrIpAddr & mask);
+	}
+
+	public static void main(String[] args) {
+		System.out.println(isInRange("192.168.1.1", "192.168.0.0/22"));
+		System.out.println(isInRange("192.168.1.1", "192.168.0.0/23"));
+		System.out.println(isInRange("192.168.1.1", "192.168.0.0/24"));
+		System.out.println(isInRange("192.168.1.1", "192.168.0.0/32"));
+	}
 }
