4138219c9982a16629e9b3d4d05b68212089fcc3
helin <juebanlin@gmail.com>
2020-04-02 16:02:55 +0800

    1

--- util4j/src/main/java/net/jueb/util4j/util/Util.java
+++ util4j/src/main/java/net/jueb/util4j/util/Util.java
@@ -1,5 +1,11 @@
 package net.jueb.util4j.util;
 
+import org.apache.commons.lang.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
 public class Util {
 
 	/**
@@ -13,7 +19,7 @@ public class Util {
 		return (number & (0x1<<pos))>>>pos;
 	}
 
-	public static boolean isInRange(String ip, String cidr) {
+	public static boolean ipIsInRange(String ip, String cidr) {
 		String[] ips = ip.split("\\.");
 		int ipAddr = (Integer.parseInt(ips[0]) << 24)
 				| (Integer.parseInt(ips[1]) << 16)
@@ -31,10 +37,55 @@ public class Util {
 		return (ipAddr & mask) == (cidrIpAddr & mask);
 	}
 
-	public static void main(String[] args) {
-		System.out.println(isInRange("192.168.1.1", "192.168.0.0/22"));
-		System.out.println(isInRange("192.168.1.1", "192.168.0.0/23"));
-		System.out.println(isInRange("192.168.1.1", "192.168.0.0/24"));
-		System.out.println(isInRange("192.168.1.1", "192.168.0.0/32"));
+	public static boolean ipMatches(String address,String ipAddressOrCidr)throws Exception{
+		int nMaskBits;
+		if (ipAddressOrCidr.indexOf('/') > 0) {
+			String[] addressAndMask = StringUtils.split(ipAddressOrCidr, "/");
+			ipAddressOrCidr = addressAndMask[0];
+			nMaskBits = Integer.parseInt(addressAndMask[1]);
+		}
+		else {
+			nMaskBits = -1;
+		}
+		InetAddress requiredAddress = InetAddress.getByName(ipAddressOrCidr);
+		InetAddress remoteAddress = InetAddress.getByName(address);
+		if (!requiredAddress.getClass().equals(remoteAddress.getClass())) {
+			return false;
+		}
+		if (nMaskBits < 0) {
+			return remoteAddress.equals(requiredAddress);
+		}
+		byte[] remAddr = remoteAddress.getAddress();
+		byte[] reqAddr = requiredAddress.getAddress();
+		int oddBits = nMaskBits % 8;
+		int nMaskBytes = nMaskBits / 8 + (oddBits == 0 ? 0 : 1);
+		byte[] mask = new byte[nMaskBytes];
+		Arrays.fill(mask, 0, oddBits == 0 ? mask.length : mask.length - 1, (byte) 0xFF);
+
+		if (oddBits != 0) {
+			int finalByte = (1 << oddBits) - 1;
+			finalByte <<= 8 - oddBits;
+			mask[mask.length - 1] = (byte) finalByte;
+		}
+		for (int i = 0; i < mask.length; i++) {
+			if ((remAddr[i] & mask[i]) != (reqAddr[i] & mask[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
+
+	public static void main(String[] args)throws Exception {
+		System.out.println(ipIsInRange("192.168.1.1", "192.168.0.0/22"));
+		System.out.println(ipIsInRange("192.168.1.1", "192.168.0.0/23"));
+		System.out.println(ipIsInRange("192.168.1.1", "192.168.0.0/24"));
+		System.out.println(ipIsInRange("192.168.1.1", "192.168.0.0/32"));
+		System.out.println(ipMatches("192.168.1.1", "192.168.1.1"));
+		System.out.println(ipMatches("192.168.1.1", "192.168.0.0/23"));
+		System.out.println(ipMatches("192.168.1.1", "192.168.0.0/24"));
+		System.out.println(ipMatches("192.168.1.1", "192.168.0.0/32"));
+	}
+
+
+
 }
